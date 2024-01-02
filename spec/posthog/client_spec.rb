d5f89719894484d3bf91ba1393f4a29360b4c931
Neil Kakkar <neilkakkar@gmail.com>
2024-01-02 13:56:40 +0100

    feat(flags): Add local props and flags to all calls

--- spec/posthog/client_spec.rb
+++ spec/posthog/client_spec.rb
@@ -554,6 +554,58 @@ class PostHog
 
         expect(client.is_feature_enabled('some_key', 'some id')).to eq(true)
       end
+
+      it 'default properties get added properly' do
+        client = Client.new(api_key: API_KEY, test_mode: true)
+
+        stub_request(:post, decide_endpoint)
+        .to_return(status: 200, body: {"featureFlags": {"beta-feature": "random-variant", "alpha-feature": true, "off-feature": false}, "featureFlagPayloads": {}}.to_json)
+
+        client.get_feature_flag('random_key', 'some_id', groups: {"company" => "id:5", "instance" => "app.posthog.com"}, person_properties: {"x1" => "y1" }, group_properties: {"company" => {"x" => "y"}})
+        assert_requested :post, decide_endpoint, times: 1
+        expect(WebMock).to have_requested(:post, decide_endpoint).with(
+          body: {"distinct_id": "some_id", "groups": {"company": "id:5", "instance": "app.posthog.com"}, "group_properties": {
+            "company": {"$group_key": "id:5", "x": "y"},
+            "instance": {"$group_key": "app.posthog.com"}
+          }, "person_properties": {"$current_distinct_id": "some_id", "x1": "y1"}, "token": "testsecret"})
+        
+        WebMock.reset_executed_requests!
+
+        client.get_feature_flag('random_key', 'some_id', groups: {"company" => "id:5", "instance" => "app.posthog.com"}, person_properties: {"$current_distinct_id" => "override" }, group_properties: {"company" => {"$group_key" => "group_override"}})
+        assert_requested :post, decide_endpoint, times: 1
+        expect(WebMock).to have_requested(:post, decide_endpoint).with(
+          body: {"distinct_id": "some_id", "groups": {"company": "id:5", "instance": "app.posthog.com"}, "group_properties": {
+            "company": {"$group_key": "group_override"},
+            "instance": {"$group_key": "app.posthog.com"}
+          }, "person_properties": {"$current_distinct_id": "override"}, "token": "testsecret"})
+        WebMock.reset_executed_requests!
+
+        # test nones
+        client.get_all_flags_and_payloads('some_id', groups: {}, person_properties: nil, group_properties: nil)
+        assert_requested :post, decide_endpoint, times: 1
+        expect(WebMock).to have_requested(:post, decide_endpoint).with(
+          body: {"distinct_id": "some_id", "groups": {}, "group_properties": {}, "person_properties": {"$current_distinct_id": "some_id"}, "token": "testsecret"})
+        WebMock.reset_executed_requests!
+
+        client.get_all_flags('some_id', groups: {"company" => "id:5"}, person_properties: nil, group_properties: nil)
+        assert_requested :post, decide_endpoint, times: 1
+        expect(WebMock).to have_requested(:post, decide_endpoint).with(
+          body: {"distinct_id": "some_id", "groups": {"company": "id:5"}, "group_properties": {"company": {"$group_key": "id:5"}}, "person_properties": {"$current_distinct_id": "some_id"}, "token": "testsecret"})
+        WebMock.reset_executed_requests!
+
+        client.get_feature_flag_payload('random_key', 'some_id', groups: {}, person_properties: nil, group_properties: nil)
+        assert_requested :post, decide_endpoint, times: 1
+        expect(WebMock).to have_requested(:post, decide_endpoint).with(
+          body: {"distinct_id": "some_id", "groups": {}, "group_properties": {}, "person_properties": {"$current_distinct_id": "some_id"}, "token": "testsecret"})
+        WebMock.reset_executed_requests!
+
+        client.is_feature_enabled('random_key', 'some_id', groups: {}, person_properties: nil, group_properties: nil)
+        assert_requested :post, decide_endpoint, times: 1
+        expect(WebMock).to have_requested(:post, decide_endpoint).with(
+          body: {"distinct_id": "some_id", "groups": {}, "group_properties": {}, "person_properties": {"$current_distinct_id": "some_id"}, "token": "testsecret"})
+        WebMock.reset_executed_requests!
+      end
+
     end
 
     context 'common' do
