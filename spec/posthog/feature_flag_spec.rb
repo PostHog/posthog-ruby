6d0d0550790c5efff3b2e11e451b5e4b7472c512
Haven Barnes <haven@posthog.com>
2025-02-12 15:22:00 -0800

    fix test

--- spec/posthog/feature_flag_spec.rb
+++ spec/posthog/feature_flag_spec.rb
@@ -3998,6 +3998,8 @@ class PostHog
           :get,
           "https://app.posthog.com/api/projects/@current/feature_flags/#{encrypted_payload_flag_key}/remote_config/"
         ).to_return(status: 200, body: mock_decrypted_payload)
+        stub_request(:post, decide_endpoint)
+        .to_return(status: 200, body:{}.to_json)
 
         c = Client.new(api_key: API_KEY, personal_api_key: API_KEY, test_mode: true)
 
