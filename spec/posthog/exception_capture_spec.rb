0208cfbefc8f2d9a526f2ea5940302989e695673
ablaszkiewicz <kqmdjc8@gmail.com>
2025-09-22 13:34:06 +0200

    feat: wip

--- spec/posthog/exception_capture_spec.rb
+++ spec/posthog/exception_capture_spec.rb
@@ -4,9 +4,9 @@ require 'spec_helper'
 
 module PostHog
   describe ExceptionCapture do
-    describe '.parse_backtrace_line' do
+    describe '#parse_backtrace_line' do
       it 'parses stacktrace line into frame with correct details' do
-        line = '/path/to/project/app/models/user.rb:42:in `validate_email\''
+        line = '/path/to/project/app/models/user.rb:42:in \'validate_email\''
         frame = described_class.parse_backtrace_line(line)
 
         expect(frame['filename']).to eq('user.rb')
@@ -18,95 +18,65 @@ module PostHog
       end
 
       it 'identifies gem files correctly' do
-        gem_line = '/path/to/gems/ruby-3.0.0/lib/ruby/gems/3.0.0/gems/some_gem/lib/some_gem.rb:10:in `gem_method\''
+        gem_line = '/path/to/gems/ruby-3.0.0/lib/ruby/gems/3.0.0/gems/some_gem/lib/some_gem.rb:10:in \'gem_method\''
         frame = described_class.parse_backtrace_line(gem_line)
 
         expect(frame['in_app']).to be false
       end
     end
 
-    describe '.add_context_lines' do
+    describe '#add_context_lines' do
       it 'adds context lines from real exception' do
-        # Define a method that will throw an exception with more context
         def test_method_that_throws
-          # Line 1 before exception
-          puts "First line before exception"
-          # Line 2 before exception  
-          puts "Second line before exception"
-          # Line 3 before exception
-          variable_before = "some setup"
-          # Line 4 before exception
-          puts "Fourth line before exception"
-          # Line 5 before exception - this will be the last pre_context line
-          puts "Fifth line before exception"
-          raise StandardError, 'Test exception for context'  # This line will be the context_line
-          # Line 1 after exception (unreachable)
-          puts "First line after exception"
-          # Line 2 after exception (unreachable)
-          puts "Second line after exception"  
-          # Line 3 after exception (unreachable)
-          puts "Third line after exception"
-          # Line 4 after exception (unreachable)
-          puts "Fourth line after exception"
-          # Line 5 after exception (unreachable)
-          puts "Fifth line after exception"
+          # -5
+          # -4
+          # -3
+          # -2
+          # -1
+          raise StandardError, 'Test exception for context'
+          # +1
+          # +2
+          # +3
+          # +4
+          # +5
         end
         
         begin
           test_method_that_throws
         rescue => e
-          # Get the first frame from this file (not from gems)
-          backtrace_line = e.backtrace.find { |line| line.include?(__FILE__) }
+          backtrace_line = e.backtrace.first
           
-          # Parse the backtrace line to get file and line number
           frame = described_class.parse_backtrace_line(backtrace_line)
           expect(frame).not_to be_nil
           
-          # Test adding context lines with default context size (5 lines before/after)
           described_class.add_context_lines(frame, frame['abs_path'], frame['lineno'])
           
-          
-          # Verify the context line (the actual exception line)
           expect(frame['context_line']).to include('raise StandardError')
           
-          # Verify pre_context has the correct lines (the 5 lines immediately before)
           expect(frame['pre_context']).to be_an(Array)
           expect(frame['pre_context'].length).to eq(5)
-          expect(frame['pre_context'][0]).to include('variable_before = "some setup"')  # Line 1
-          expect(frame['pre_context'][1]).to include('# Line 4 before exception')      # Line 2
-          expect(frame['pre_context'][2]).to include('puts "Fourth line before exception"') # Line 3
-          expect(frame['pre_context'][3]).to include('# Line 5 before exception')      # Line 4  
-          expect(frame['pre_context'][4]).to include('puts "Fifth line before exception"') # Line 5
+          expect(frame['pre_context'][0]).to include('# -5')
+          expect(frame['pre_context'][1]).to include('# -4')
+          expect(frame['pre_context'][2]).to include('# -3')
+          expect(frame['pre_context'][3]).to include('# -2')
+          expect(frame['pre_context'][4]).to include('# -1')
           
-          # Verify post_context has lines after the exception  
           expect(frame['post_context']).to be_an(Array)
           expect(frame['post_context'].length).to eq(5)
-          expect(frame['post_context'][0]).to include('# Line 1 after exception')     # Line 1
-          expect(frame['post_context'][1]).to include('puts "First line after exception"') # Line 2
-          expect(frame['post_context'][2]).to include('# Line 2 after exception')     # Line 3
-          expect(frame['post_context'][3]).to include('puts "Second line after exception"') # Line 4
-          expect(frame['post_context'][4]).to include('# Line 3 after exception')     # Line 5
-          
-          # Test with custom context size (2 lines)
-          frame_small = described_class.parse_backtrace_line(backtrace_line)
-          described_class.add_context_lines(frame_small, frame_small['abs_path'], frame_small['lineno'], 2)
-          
-          
-          expect(frame_small['pre_context'].length).to eq(2)
-          expect(frame_small['post_context'].length).to eq(2)
-          expect(frame_small['pre_context'][0]).to include('# Line 5 before exception')
-          expect(frame_small['pre_context'][1]).to include('puts "Fifth line before exception"')
-          expect(frame_small['post_context'][0]).to include('# Line 1 after exception')
-          expect(frame_small['post_context'][1]).to include('puts "First line after exception"')
+          expect(frame['post_context'][0]).to include('# +1')
+          expect(frame['post_context'][1]).to include('# +2')
+          expect(frame['post_context'][2]).to include('# +3')
+          expect(frame['post_context'][3]).to include('# +4')
+          expect(frame['post_context'][4]).to include('# +5')
         end
       end
     end
 
-    describe '.build_stacktrace' do
+    describe '#build_stacktrace' do
       it 'converts backtrace array to structured frames' do
         backtrace = [
-          '/path/to/project/app/models/user.rb:42:in `validate_email\'',
-          '/path/to/gems/ruby-3.0.0/lib/ruby/gems/3.0.0/gems/actionpack-7.0.0/lib/action_controller.rb:123:in `dispatch\''
+          '/path/to/project/app/models/user.rb:42:in \'validate_email\'',
+          '/path/to/gems/ruby-3.0.0/lib/ruby/gems/3.0.0/gems/actionpack-7.0.0/lib/action_controller.rb:123:in '\dispatch\''
         ]
         
         stacktrace = described_class.build_stacktrace(backtrace)
@@ -119,7 +89,7 @@ module PostHog
       end
     end
 
-    describe '.build_exception_properties' do
+    describe '#build_exception_properties' do
       it 'creates structured exception data' do
         begin
           raise StandardError, 'Test error message'
